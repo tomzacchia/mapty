503b83671fc2478d0ddb28c11b5b06f492b74e4d
tomzacchia <tomzacchia@live.com>
2020-11-02 20:06:42 -0500

    Get general user location on load

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
-'use strict';
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
-// prettier-ignore
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const form = document.querySelector(".form");
+const containerWorkouts = document.querySelector(".workouts");
+const inputType = document.querySelector(".form__input--type");
+const inputDistance = document.querySelector(".form__input--distance");
+const inputDuration = document.querySelector(".form__input--duration");
+const inputCadence = document.querySelector(".form__input--cadence");
+const inputElevation = document.querySelector(".form__input--elevation");
 
-const form = document.querySelector('.form');
-const containerWorkouts = document.querySelector('.workouts');
-const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--distance');
-const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function getUserCoordinates(position) {
+      const { latitude, longitude } = position.coords;
+      console.log(`https://www.google.pt/maps/@${latitude},${longitude},14z`);
+    },
+    function () {
+      alert("could not get your position");
+    }
+  );
+}
